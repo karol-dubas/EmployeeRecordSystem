f107b33adaac31964cc6cc107033404402842633
Karol Dubas <krizz.kontakt@gmail.com>
2022-02-10 19:14:05 +0100

    Moved entity configurations

---
+++ MVC FirstApp/Data/Configurations/PositionConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MVC_FirstApp.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_FirstApp.Data.Configurations
+{
+    public class PositionConfiguration : IEntityTypeConfiguration<Position>
+    {
+        public void Configure(EntityTypeBuilder<Position> builder)
+        {
+            builder.Property(c => c.Name).IsRequired();
+        }
+    }
+}
--- MVC FirstApp/Data/MvcDbContext.cs
+++ MVC FirstApp/Data/MvcDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using MVC_FirstApp.Data.Configurations;
 using MVC_FirstApp.Data.Entities;
 using System;
 using System.Collections.Generic;
@@ -23,26 +24,9 @@ namespace MVC_FirstApp.Data
         {
             base.OnModelCreating(builder);
 
-            builder.Entity<User>()
-                .ToTable("Users") // rename table
-                .Ignore(c => c.TwoFactorEnabled)
-                .Ignore(c => c.PhoneNumberConfirmed)
-                .Ignore(c => c.PhoneNumber)
-                .Ignore(c => c.NormalizedEmail)
-                .Ignore(c => c.LockoutEnd)
-                .Ignore(c => c.LockoutEnabled)
-                .Ignore(c => c.EmailConfirmed)
-                .Ignore(c => c.Email)
-                .Ignore(c => c.AccessFailedCount);
-
-            builder.Entity<User>().Property(c => c.FirstName).IsRequired();
-            builder.Entity<User>().Property(c => c.LastName).IsRequired();
-            builder.Entity<User>().Property(c => c.UserName).IsRequired();
-            builder.Entity<User>().Property(c => c.PasswordHash).IsRequired();
-
-            builder.Entity<Position>().Property(c => c.Name).IsRequired();
-
-            builder.Entity<Group>().Property(c => c.Name).IsRequired();
+            builder.ApplyConfiguration(new GroupConfiguration());
+            builder.ApplyConfiguration(new PositionConfiguration());
+            builder.ApplyConfiguration(new UserConfiguration());
         }
     }
 }
