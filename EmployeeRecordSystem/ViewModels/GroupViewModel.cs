2df4d9b8371c0c19fdf2141a800034fe05522754
Karol Dubas <krizz.kontakt@gmail.com>
2022-06-06 19:59:27 +0200

    Clean solution

--- EmployeeRecordSystem/ViewModels/GroupViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EmployeeRecordSystem.ViewModels
-{
-    public class GroupViewModel
-    {
-        public long Id { get; set; }
-        public string Name { get; set; }
-        public bool IsCurrent { get; set; }
-    }
-}
