31d42d4508d1b85ad16669297147b7d436c6e7a3
Karol Dubas <krizz.kontakt@gmail.com>
2022-12-21 19:02:33 +0100

    Add client validation

---
+++ src/Client/Validators/EmployeeDetailsDtoValidator.cs
@@ -0,0 +1,31 @@
+﻿using EmployeeRecordSystem.Shared.Responses;
+using FluentValidation;
+
+namespace EmployeeRecordSystem.Client.Validators;
+
+public class EmployeeDetailsDtoValidator : AbstractValidator<EmployeeDetailsDto>
+{
+	public EmployeeDetailsDtoValidator()
+	{
+		RuleFor(e => e.FirstName)
+			.MaximumLength(50).WithMessage("Maksymalna długość to 50 znaków")
+			.NotEmpty().WithMessage("Pole nie może być puste");
+
+		RuleFor(e => e.LastName)
+			.MaximumLength(50).WithMessage("Maksymalna długość to 50 znaków")
+			.NotEmpty().WithMessage("Pole nie może być puste");
+
+		RuleFor(e => e.BankAccountNumber)
+			.MaximumLength(34).WithMessage("Maksymalna długość to 34 znaki");
+
+		RuleFor(e => e.Note)
+			.MaximumLength(300).WithMessage("Maksymalna długość to 300 znaków");
+	}
+
+	public Func<object, string, Task<IEnumerable<string>>> ValidateValue => async (model, propertyName) =>
+	{
+		var result = await ValidateAsync(
+			ValidationContext<EmployeeDetailsDto>.CreateWithOptions((EmployeeDetailsDto)model, x => x.IncludeProperties(propertyName)));
+		return result.IsValid ? Array.Empty<string>() : result.Errors.Select(e => e.ErrorMessage);
+	};
+}
---
+++ src/Client/Validators/FluentValueValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+
+namespace EmployeeRecordSystem.Client.Validators;
+
+/// <summary>
+/// A glue class to make it easy to define validation rules for single values using FluentValidation.
+/// </summary>
+/// <typeparam name="T">Type of variable to be validated</typeparam>
+public class FluentValueValidator<T> : AbstractValidator<T>
+{
+	public FluentValueValidator(Action<IRuleBuilderInitial<T, T>> rule)
+	{
+		rule(RuleFor(x => x));
+	}
+
+	private IEnumerable<string> ValidateValue(T arg)
+	{
+		var result = Validate(arg);
+		return result.IsValid ? Array.Empty<string>() : result.Errors.Select(e => e.ErrorMessage);
+	}
+
+	public Func<T, IEnumerable<string>> Validation => ValidateValue;
+}
