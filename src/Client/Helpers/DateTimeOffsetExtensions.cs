82e85234040b0a7cf81013223bacf4280c393fe5
Karol Dubas <krizz.kontakt@gmail.com>
2023-02-05 19:54:41 +0100

    Update date: display format & default sorting 📆

---
+++ src/Client/Helpers/DateTimeOffsetExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeeRecordSystem.Client.Helpers;
+
+public static class DateTimeOffsetExtensions
+{
+	public static string Humanize(this DateTimeOffset date)
+	{
+		return date.ToLocalTime()
+			.ToString("dd.MM.yyyy HH:mm");
+	}
+}
